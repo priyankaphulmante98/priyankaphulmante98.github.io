3de0c8d8cb64c425cabf6005b03d15bf235ae00e
priyanka <priyankaphulmante98@gmail.com>
2022-10-13 14:22:19 +0530

    3

---
+++ PORTFOILO/project/project.js
@@ -0,0 +1,291 @@
+const Projects = [
+    {
+      name: "Clone of Expedia.com",
+      about:"Expedia web applications is to helps travelers easily plan and book their whole trip with the widest selection of vacation packages,flights, hotels, vacation rentals,rental cars, cruises, activities attractive and sevices.",
+      img: "./images/expedia.png",
+      gitrepo: "https://github.com/priyankaphulmante98/Expedia_Clone",
+      video: "https://whimsical-druid-3a9294.netlify.app/",
+      blog: "https://medium.com/@priyankaphulmante98/expedia-clone-project-a854078f217c",
+      tech_stack: "HTML | CSS | JAVASCRIPT",
+      tech_stackicons: [
+        `<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+        `<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+        `<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+      ],
+      project_link: "https://whimsical-druid-3a9294.netlify.app/",
+      project_type: "Team Project",
+      category: ["all", "html"],
+    },
+    {
+      name: "Clone of Kindmeal.my",
+      about:
+        "Kindmeal.my is  Malaysian's leading meat-free lifestyle platform, indulging you with delicious discounted vegetarian meals at cost environment we aim to encourage people in reducing meat consumption saving precious animal lives, health , environment and money with testy meals.",
+      img: "./images/kindmeal.png",
+      gitrepo: "https://github.com/raushanraj901/Unit_3_Project_kindmeal",
+      video: "https://fantastic-horse-7681f8.netlify.app/",
+      blog: "https://medium.com/p/fc6c31f7e78f/",
+      tech_stack: "HTML | CSS | JAVASCRIPT | API's",
+      tech_stackicons: [
+        `<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+        `<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+        `<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+        `<img src="https://img.icons8.com/nolan/64/api-settings.png" width="48px"/>`,
+      ],
+      project_link: "https://fantastic-horse-7681f8.netlify.app/",
+      project_type: "Team Project",
+      category: ["all", "html"],
+    },
+    {
+      name: "Clone of YouTube",
+      about:
+        "A frontend clone of YouTube, where in the home page user will find popular videos of YouTube and also can search for the desired videos it will show all the videos related to search on the result page.",
+      img: "./images/Youtube-clone.png",
+      gitrepo: "https://github.com/priyankaphulmante98/YOUTUBE-CLONE",
+      video: "https://youtube-clone-eight-navy.vercel.app/",
+      blog: "https://medium.com/@priyankaphulmante98/youtube-clone-6f34318fc7e9",
+      tech_stack: "HTML | CSS | JAVASCRIPT | API's",
+      tech_stackicons: [
+        `<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+        `<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+        `<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+        `<img src="https://img.icons8.com/nolan/64/api-settings.png" width="48px"/>`,
+      ],
+      project_link: "https://youtube-clone-eight-navy.vercel.app/",
+      project_type: "Indivisual Project",
+      category: ["all", "html"],
+    },
+    {
+        name: "clone of Shapelink fitness website",
+        about:
+          "Shapelink website is a workout log website which helps us to track our goals and schedules. it also has an Android app, however with different name Twiik.",
+        img: "./images/shapelink.png",
+        gitrepo: "https://github.com/priyankaphulmante98/TwikkFitness-App_Clone",
+        video: "https://jovial-medovik-e92b37.netlify.app/",
+        blog: "",
+        tech_stack:
+          " HTML | CSS | JavaScript | Local-Storage | ES6",
+        tech_stackicons: [
+          `<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+          `<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+          `<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+          `<img src="https://img.icons8.com/color/48/000000/express.png"/>`,
+          `<img src="https://img.icons8.com/color/48/000000/mongodb.png"/>`,
+          `<img src="https://img.icons8.com/windows/48/000000/node-js.png"/>`,
+        ],
+        project_link: "https://jovial-medovik-e92b37.netlify.app/",
+        project_type: "indiviusal Project",
+        category: ["all", "html", "other"],
+      },
+    {
+      name: "Clone of beautyplus.com",
+      about:
+        "beautyplus is a beauty retail companythat sells cosmetic commodities online. In this project.",
+      img: "./images/Beauty_plus.png",
+      gitrepo: "https://github.com/priyankaphulmante98/beautyplus-main",
+      video: "",
+      blog: "https://beautyplus-main.vercel.app/",
+      tech_stack:
+        " HTML | CSS | JavaScript | React.js | Redux ",
+      tech_stackicons: [
+        `<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+        `<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+        `<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+        `<img src="https://img.icons8.com/color/48/000000/express.png"/>`,
+        `<img src="https://img.icons8.com/color/48/000000/mongodb.png"/>`,
+        `<img src="https://img.icons8.com/windows/48/000000/node-js.png"/>`,
+      ],
+      project_link: "https://beautyplus-main.vercel.app/",
+      project_type: "Indiviusal Project",
+      category: ["all", "react"],
+    },
+    {
+      name: "DNA-India News website clone",
+      about:
+        "dna India is basically news_website. Through news, views, analyses and interactivity, dna provides readers with a composite unbiased picture of the city, the country and the world around them.",
+      img: "./images/DnaIndia.png",
+      gitrepo: "https://github.com/priyankaphulmante98/DNA_India_News_App_using-JS-",
+      video: "https://dna-india-news-app-using-js-eil6.vercel.app/",
+      blog: "",
+      tech_stack:
+        "HTML | CSS | JAVASCRIPT | React | Redux | NPM | styles Components",
+      tech_stackicons: [
+        `<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+        `<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+        `<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+        `<img src="https://img.icons8.com/color/50/000000/react-native.png"/>`,
+        // `<img src="https://img.icons8.com/color/48/000000/mongodb.png"/>`,
+        // `<img src="https://img.icons8.com/color/48/000000/express.png"/>`,
+        // `<img src="https://img.icons8.com/windows/48/000000/node-js.png"/>`,
+      ],
+      project_link: "https://dna-india-news-app-using-js-eil6.vercel.app/",
+      project_type: "Indivisual Project",
+      category: ["all", "react", "html"],
+    },
+    {
+      name: "TIC-TAC-TOE APP",
+      about:
+        " Tic Tac Toe is well known game that is simple to grasp, easy to adapt and excellent for building beginner planning, problem solving and perspective taking skills. ",
+      img: "./images/tic-tac-toe.png",
+      gitrepo: "https://github.com/priyankaphulmante98/React-tic-tac-toe-app",
+      video: "https://game-sepia.vercel.app/",
+      blog: "https://medium.com/@priyankaphulmante98/use-react-to-make-tic-tac-toe-in-5-steps-8b8b8fde1178",
+      tech_stack:
+        "HTML | CSS | JAVASCRIPT | React | Redux | NPM",
+      tech_stackicons: [
+        `<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+        `<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+        `<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+        `<img src="https://img.icons8.com/color/50/000000/react-native.png"/>`,
+        `<img src="https://img.icons8.com/color/48/000000/redux.png"/>`,
+        // `<img src="https://img.icons8.com/color/48/000000/mongodb.png"/>`,
+        // `<img src="https://img.icons8.com/color/48/000000/express.png"/>`,
+        // `<img src="https://img.icons8.com/windows/48/000000/node-js.png"/>`,
+      ],
+      project_link: "https://game-sepia.vercel.app/",
+      project_type: "Indiviusal Project",
+      category: ["all", "react"],
+    },
+    {
+      name: "Google Timer stopwatch",
+      about:
+        "App timer is an incredibly useful tool for time management if you are someone who struggles to get off an app. It can help you save time and become more disciplined about how you spend it. Done wisely, it can also help boost your productivity, reduce stress, and help with practicing digital detox.",
+      img: "./images/google.png",
+      gitrepo: "https://github.com/priyankaphulmante98/React-Google-Timer-Stopwatch",
+      video: "https://google-five-lake.vercel.app/",
+      blog: "https://medium.com/@priyankaphulmante98/building-timers-in-react-stopwatch-and-countdown-7b8470e9b5a",
+      tech_stack: "HTML | CSS | JAVASCRIPT | React | Material UI | Bootstrap | NPM",
+      tech_stackicons: [
+        `<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+        `<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+        `<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+        `<img src="https://img.icons8.com/color/50/000000/react-native.png"/>`,
+        `<img src="https://img.icons8.com/color/50/000000/material-ui.png"/>`,
+        `<img src="https://img.icons8.com/nolan/64/api-settings.png" width="48px"/>`,
+      ],
+      project_link: "https://google-five-lake.vercel.app/",
+      project_type: "Indivisual Project",
+      category: ["all", "react"],
+    },
+    {
+      name: "TODO -Schedule Reminder App",
+      about:
+        "TODO is a React based web application. Here user can schedule there daily task with alarm reminder, also can see previous tasks done and user can save important urls for future use and easy to find.",
+      img: "./images/todo-app.png",
+      gitrepo: "https://github.com/priyankaphulmante98/REACT-TODO-APP",
+      video: "https://todo-app-sigma-gray.vercel.app/",
+      blog: "https://medium.com/@priyankaphulmante98/how-to-build-a-todo-app-23ddbe667713",
+      tech_stack:
+        "HTML | CSS | JAVASCRIPT | React | Redux | NPM",
+      tech_stackicons: [
+        `<img src="https://img.icons8.com/color/50/000000/html-5--v1.png" />`,
+        `<img src="https://img.icons8.com/color/50/000000/css3.png"/>`,
+        `<img src="https://img.icons8.com/color/50/000000/javascript--v2.png"/>`,
+        `<img src="https://img.icons8.com/color/50/000000/react-native.png"/>`,
+        // `<img src="https://img.icons8.com/color/48/000000/mongodb.png"/>`,
+        // `<img src="https://img.icons8.com/color/48/000000/express.png"/>`,
+        // `<img src="https://img.icons8.com/windows/48/000000/node-js.png"/>`,
+      ],
+      project_link: "https://todo-app-sigma-gray.vercel.app/",
+      project_type: "Indivisual Project",
+      category: ["all", "react"],
+    },
+   
+    
+  
+  
+  ];
+  
+  let projects__container = document.getElementById("projects--container");
+  
+  let all_projects = document.querySelector(".all_projects");
+  let react_projects = document.querySelector(".react_projects");
+  let mern_projects = document.querySelector(".mern_projects");
+  // let other_projects = document.querySelector(".other_projects");
+  let html_projects = document.querySelector(".html_projects");
+  all_projects.classList.add("active_tech_project");
+  
+  all_projects.addEventListener("click", () => {
+    filterProjects("all");
+    all_projects.classList.add("active_tech_project");
+    react_projects.classList.remove("active_tech_project");
+    mern_projects.classList.remove("active_tech_project");
+    // other_projects.classList.remove("active_tech_project")
+    html_projects.classList.remove("active_tech_project");
+  });
+  react_projects.addEventListener("click", () => {
+    filterProjects("react");
+    all_projects.classList.remove("active_tech_project");
+    react_projects.classList.add("active_tech_project");
+    mern_projects.classList.remove("active_tech_project");
+    // other_projects.classList.remove("active_tech_project")
+    html_projects.classList.remove("active_tech_project");
+  });
+  mern_projects.addEventListener("click", () => {
+    filterProjects("mern");
+    all_projects.classList.remove("active_tech_project");
+    react_projects.classList.remove("active_tech_project");
+    mern_projects.classList.add("active_tech_project");
+    // other_projects.classList.remove("active_tech_project")
+    html_projects.classList.remove("active_tech_project");
+  });
+  // other_projects.addEventListener("click", () => {
+  //     filterProjects("other");
+  //     all_projects.classList.remove("active_tech_project")
+  //     react_projects.classList.remove("active_tech_project")
+  //     mern_projects.classList.remove("active_tech_project")
+  //     // other_projects.classList.add("active_tech_project")
+  //     html_projects.classList.remove("active_tech_project")
+  // })
+  html_projects.addEventListener("click", () => {
+    filterProjects("html");
+    all_projects.classList.remove("active_tech_project");
+    react_projects.classList.remove("active_tech_project");
+    mern_projects.classList.remove("active_tech_project");
+    // other_projects.classList.remove("active_tech_project")
+    html_projects.classList.add("active_tech_project");
+  });
+  
+  function filterProjects(basis) {
+    if (basis === "all") {
+      let filtered = Projects.filter((el) => {
+        return el.category.includes(basis);
+      });
+      displayProjectData(filtered.reverse());
+    } else {
+      let filtered = Projects.filter((el) => {
+        return el.category.includes(basis);
+      });
+      displayProjectData(filtered);
+    }
+  }
+  
+  function displayProjectData(Projects) {
+    projects__container.innerHTML = null;
+    Projects.reverse().forEach((pro) => {
+      let main = document.createElement("div");
+      main.setAttribute("class", "portfolio-item padd-15");
+      main.innerHTML = `
+      <div class="portfolio-item-inner shadow-dark">
+      <div class="portfolio-img">
+        <img src=${pro.img} alt="">
+        <h3 class="project-name">${pro.name}</h3>
+        <h4 class="project-name project__about">${pro.about}</h4>
+        <div class="navigate_ar">
+        <a href=${pro.blog} target="_blank" title="Blog" class="blog-post"><i class="fab fa-blogger-b"></i></a>
+        <a href=${pro.project_link} target="_blank" title="Demo" class="see-project"><i class="fas fa-eye"></i></a>
+        <a href=${pro.gitrepo} target="_blank" title="GitHub Repository" class="github-repo"><i class="fab fa-github"></i></a>
+        <a href=${pro.video} target="_blank" title="Video Demo" class="video"><i class="fas fa-video"></i></a>
+        </div>
+        <div class="project_tech_box">
+        <span class="TechStacks__text">TechStacks: </span> ${pro.tech_stack}
+        </div>
+        
+      </div>
+    </div>
+      `;
+      projects__container.append(main);
+    });
+  }
+  
+  displayProjectData(Projects);
+  
